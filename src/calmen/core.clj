cf9a3010cbc3f6a947592456172796413ad80aa1
Akira Ota 10nin <akira19870221@gmail.com>
2018-03-01 20:07:39 +0900

    Add scraping functions for get calendar month, and generate keyword for parser.

--- src/calmen/core.clj
+++ src/calmen/core.clj
@@ -4,7 +4,17 @@
 (defn fetch-url [url]
   (html/html-resource (java.net.URL. url)))
 
-(defn get-event-month [lib-html-root]
-  (html/select lib-html-root [:table.EventYear_EventCalendar]))
+(defn get-event-month [event-month-node]
+  (:content (first (html/select event-month-node [:td.EventTitle :table :tr :td]))))
 
-(def *lib-html-root* (html/select (fetch-url "http://www.library.metro.tokyo.jp/guide/central_library/tabid/1410/Default.aspx") [:table.EventMain]))
+(defn gen-EventYearAttributes []
+  (let [attr-base "table#dnn_ctr12401_Events_EventYear_EventCalendar"]
+    (for [x (range 1 13)]  (keyword (str attr-base x)))))
+
+(defn get-EventYearCalendar [url]
+  (html/select (fetch-url url) [:div#EventYear_EventCalendars]))
+
+(defn get-EventMonth [event-year-calendar]
+  (let [month-keys (gen-EventYearAttributes)]
+    nil)
+;(def *lib-html-root* (html/select (fetch-url "http://www.library.metro.tokyo.jp/guide/central_library/tabid/1410/Default.aspx") [:table.EventMain]))
