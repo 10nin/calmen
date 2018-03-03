d2b64ddd82c98ce769b94d9e88e31ec69dff5ffe
Akira Ota 10nin <akira19870221@gmail.com>
2018-03-03 22:27:13 +0900

    Del fetch-url function, implement get closing days information from parsed data, and minimum refactoring.

--- src/calmen/core.clj
+++ src/calmen/core.clj
@@ -1,21 +1,21 @@
 (ns calmen.core
   (:require [net.cgrand.enlive-html :as html]))
 
-(defn fetch-url [url]
-  (html/html-resource (java.net.URL. url)))
-
-(defn get-EventMonth-node [event-month-node]
-  (:content (first (html/select event-month-node [:td.EventTitle :table :tr :td]))))
+(defn get-EventMonth [event-month-node]
+  (list
+   (first (:content (first (html/select event-month-node [:td.EventTitle :table :tr :td]))))
+   (for [d (html/select event-month-node [:td.Closing])] (first (:content d)))))
 
 (defn gen-EventYearAttributes []
   (let [attr-base "table#dnn_ctr12401_Events_EventYear_EventCalendar"]
     (for [x (range 1 13)]  (keyword (str attr-base x)))))
 
 (defn get-EventYearCalendar [url]
-  (html/select (fetch-url url) [:div#EventYear_EventCalendars]))
+  (html/select (html/html-resource (java.net.URL. url)) [:div#EventYear_EventCalendars]))
 
-(defn get-EventMonths [event-year-calendar-node]
+(defn get-ClosingCalendar [event-year-calendar-node]
   (let [month-keys (gen-EventYearAttributes)]
-    (for [k month-keys] (get-EventMonth-node (html/select event-year-calendar-node [k])))))
+    (for [k month-keys] (get-EventMonth (html/select event-year-calendar-node [k])))))
 
-;"http://www.library.metro.tokyo.jp/guide/central_library/tabid/1410/Default.aspx"
+(defn -main []
+  (get-ClosingCalendar get-EventYearCalendar "http://www.library.metro.tokyo.jp/guide/central_library/tabid/1410/Default.aspx"))
