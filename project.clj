92fb3c378a719db9e3a3a4d8306025c18df94aaa
Akira Ota 10nin <akira19870221@gmail.com>
2018-03-08 22:47:21 +0900

    Add project url.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject calmen "0.1.0-SNAPSHOT"
   :description "A Tokyo Metropolitan Library annual calender getter."
-  :url ""
+  :url "https://gitlab.com/10nin/calmen"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
